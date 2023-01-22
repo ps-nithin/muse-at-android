f534289e18c5cd6d8548a960dc08b9fda50bd037
ps-nithin <nithin.pulpra@gmail.com>
2023-01-22 20:47:14 +0530

    *swipe refresh layout. *no internet handler.

--- app/src/main/java/com/messaging/muse/OpenFragment.java
+++ app/src/main/java/com/messaging/muse/OpenFragment.java
@@ -3,7 +3,9 @@ package com.messaging.muse;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +19,36 @@ import android.webkit.WebViewClient;
  */
 public class OpenFragment extends Fragment {
 
+    SwipeRefreshLayout swipeRefreshLayoutOpen;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_open,container,false);
         WebView webView=view.findViewById(R.id.open_webview_id);
+        swipeRefreshLayoutOpen=view.findViewById(R.id.refreshLayoutOpen);
         webView.loadUrl("https://www.muse-at.com/android/open.php");
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new myWebViewClient());
         // Inflate the layout for this fragment
+        swipeRefreshLayoutOpen.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayoutOpen.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayoutOpen.setRefreshing(false);
+                        webView.loadUrl("https://www.muse-at.com/android/open.php");
+                    }
+                },3000);
+            }
+        });
         return view;
+    }
+    public class myWebViewClient extends WebViewClient{
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            view.loadUrl("file:///android_asset/no_internet.html");
+        }
     }
 }
--- app/src/main/java/com/messaging/muse/WelcomeFragment.java
+++ app/src/main/java/com/messaging/muse/WelcomeFragment.java
@@ -1,14 +1,25 @@
 package com.messaging.muse;
 
+import android.graphics.Bitmap;
+import android.net.http.SslError;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import kotlin.coroutines.jvm.internal.RunSuspendKt;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,17 +28,37 @@ import android.webkit.WebViewClient;
  */
 public class WelcomeFragment extends Fragment {
 
-
+    SwipeRefreshLayout swipeRefreshLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_welcome,container,false);
         WebView webView=view.findViewById(R.id.welcome_id);
+        swipeRefreshLayout=view.findViewById(R.id.refreshLayout);
         webView.loadUrl("https://www.muse-at.com/android/welcome.php");
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-
+        webView.setWebViewClient(new myWebViewClient());
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                        webView.loadUrl("https://www.muse-at.com/android/welcome.php");
+                    }
+                },3000);
+            }
+        });
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public class myWebViewClient extends WebViewClient{
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            view.loadUrl("file:///android_asset/no_internet.html");
+        }
     }
 }
